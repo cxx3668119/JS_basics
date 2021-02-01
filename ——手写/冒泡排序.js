23534c08f2cd2df90b8789578a7a91c9f0e48477
YGL <862704268@qq.com>
2021-02-01 17:43:03 +0800

    2.01

---
+++ ——手写/冒泡排序.js
@@ -0,0 +1,13 @@
+let ary = [12, 8, 24, 16, 1];
+
+function bubble(ary) {
+  //外层循环 控制比较轮数
+  for (let i = 0; i < ary.length - 1; i++) {
+    //内层控制每一轮比较次数
+    for (let j = 0; j < ary.length - 1 - i; j++) {
+      if (ary[j] > ary[j + 1]) {
+        
+      }
+    }
+  }
+}
