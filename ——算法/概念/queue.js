534fed606d3fa653eafbcf0b772a5a9da582bec4
cxx3668119 <862704268@qq.com>
2021-09-26 15:33:53 +0800

    9.26

---
+++ ——算法/概念/queue.js
@@ -0,0 +1,11 @@
+const queue = [];
+queue.push(1)
+queue.push(2)
+queue.push(3)
+
+// 出队
+
+const item1 = queue.shift()
+const item2 = queue.shift()
+const item3 = queue.shift()
+  
---
+++ ——算法/概念/stack.js
@@ -0,0 +1,12 @@
+const stack = []
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+//123
+3
+2
+1
+const item1 = stack.pop()
+const item2 = stack.pop()
