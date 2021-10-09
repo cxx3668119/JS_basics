3292fcd1bf000061ecef81ff8a790bc8298f9f6a
cxx3668119 <862704268@qq.com>
2021-10-09 18:07:17 +0800

    reduce

--- ——手写/数组方法/index.js
+++ ——手写/数组方法/index.js
@@ -196,42 +196,123 @@ var names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
 // }, {}));
 
 // 5、按属性对object分类
-var people = [
-  { name: 'Alice', age: 21 },
-  { name: 'Max', age: 20 },
-  { name: 'Jane', age: 20 }
-];
+// var people = [
+//   { name: 'Alice', age: 21 },
+//   { name: 'Max', age: 20 },
+//   { name: 'Jane', age: 20 }
+// ];
 
-function demo(arr, key) {
-  return arr.reduce((acc, item) => {
-    let result = item[key]  //item.age
-    if (!acc[result]) {     //添加新对象key名
-      acc[result] = []  //赋值key名的值为空数组
-    }
-    acc[result].push(item)  //添加值
-    return acc
-  }, {})
+// function demo(arr, key) {
+//   return arr.reduce((acc, item) => {
+//     let result = item[key]  //item.age
+//     if (!acc[result]) {     //添加新对象key名
+//       acc[result] = []  //赋值key名的值为空数组
+//     }
+//     acc[result].push(item)  //添加值  
+//     return acc
+//   }, {})
+// }
+
+// console.log(demo(people, 'age'));
+
+// 6、使用扩展运算符和initialValue绑定包含在对象数组中的数组
+// friends - 对象数组
+// where object field "books" - list of favorite books
+var friends = [{
+  name: 'Anna',
+  books: ['Bible', 'Harry Potter'],
+  age: 21
+}, {
+  name: 'Bob',
+  books: ['War and peace', 'Romeo and Juliet'],
+  age: 26
+}, {
+  name: 'Alice',
+  books: ['The Lord of the Rings', 'The Shining'],
+  age: 18
+}];
+
+// allbooks - list which will contain all friends' books +
+// additional list contained in initialValue
+var allbooks = friends.reduce(function (prev, curr) {
+  return [...prev, ...curr.books];
+}, ['Alphabet']);
+
+// allbooks = [
+//   'Alphabet', 'Bible', 'Harry Potter', 'War and peace',
+//   'Romeo and Juliet', 'The Lord of the Rings',
+//   'The Shining'
+// ]
+
+
+// 7、数组去重
+// let myArray = ['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd']
+// let myOrderedArray = myArray.reduce(function (accumulator, currentValue) {
+//   if (accumulator.indexOf(currentValue) === -1) {
+//     accumulator.push(currentValue)
+//   }
+//   return accumulator
+// }, [])
+
+// console.log(myOrderedArray)
+
+// let arr = [1, 2, 1, 2, 3, 5, 4, 5, 3, 4, 4, 4, 4];
+// let result = arr.sort().reduce((init, current) => {
+//   if (init.length === 0 || init[init.length - 1] !== current) {
+//     init.push(current);
+//   }
+//   return init;
+// }, []);
+// console.log(result); //[1,2,3,4,5]
+
+// 8、按顺序运行Promise
+function runPromiseInSequence(arr, input) {
+  return arr.reduce(
+    (promiseChain, currentFunction) => promiseChain.then(currentFunction),
+    Promise.resolve(input)
+  );
 }
 
-console.log(demo(people, 'age'));
+// promise function 1
+function p1(a) {
+  return new Promise((resolve, reject) => {
+    resolve(a * 5);
+  });
+}
 
-// function groupBy(objectArray, property) {
-//   return objectArray.reduce(function (acc, obj) {
-//     var key = obj[property];
-//     if (!acc[key]) {
-//       acc[key] = [];
-//     }
-//     acc[key].push(obj);
-//     return acc;
-//   }, {});
-// }
+// promise function 2
+function p2(a) {
+  return new Promise((resolve, reject) => {
+    resolve(a * 2);
+  });
+}
 
-// var groupedPeople = groupBy(people, 'age');
-// groupedPeople is:
-// {
-//   20: [
-//     { name: 'Max', age: 20 },
-//     { name: 'Jane', age: 20 }
-//   ],
-//   21: [{ name: 'Alice', age: 21 }]
-// }
+// function 3  - will be wrapped in a resolved promise by .then()
+function f3(a) {
+  return a * 3;
+}
+
+// promise function 4
+function p4(a) {
+  return new Promise((resolve, reject) => {
+    resolve(a * 4);
+  });
+}
+
+const promiseArr = [p1, p2, f3, p4];
+runPromiseInSequence(promiseArr, 10)
+  .then(console.log);   // 1200
+
+// 9、使用 reduce实现map
+if (!Array.prototype.mapUsingReduce) {
+  Array.prototype.mapUsingReduce = function (callback, thisArg) {
+    return this.reduce(function (mappedArray, currentValue, index, array) {
+      mappedArray[index] = callback.call(thisArg, currentValue, index, array)
+      return mappedArray
+    }, [])
+  }
+}
+
+[1, 2, , 3].mapUsingReduce(
+  (currentValue, index, array) => currentValue + index + array.length
+) // [5, 7, , 10]
